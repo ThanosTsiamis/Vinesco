2d41e0781474ad84d0bad3346842b626843dba20
ThanosTsiamis <thatsiamis@gmail.com>
2023-10-05 19:14:50 +0300

    Added a method to bulk upload varieties

--- pages/insertNewData.py
+++ pages/insertNewData.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk, filedialog
+
 import pandas as pd
 
 
@@ -11,13 +13,17 @@ class InsertNewData(tk.Frame):
         # Entry field for user input
         self.variety_entry = tk.Entry(self)
         self.variety_entry.pack()
+
         # Button to perform the search
         search_button = tk.Button(self, text="Search", command=self.search_variety)
         search_button.pack()
 
-        welcomeButton = tk.Button(self, text="Go to Home Page", command=lambda: controller.show_frame("WelcomePage"))
-        # Put the welcome Button on the upper left side of the screen
-        welcomeButton.place(x=0, y=0)
+        # Button to upload an external file
+        upload_button = tk.Button(self, text="Upload File", command=self.load_external_file)
+        upload_button.pack()
+
+        welcome_button = tk.Button(self, text="Go to Home Page", command=lambda: controller.show_frame("WelcomePage"))
+        welcome_button.place(x=0, y=0)
 
     def search_variety(self):
         # Get the user input from the entry field
@@ -35,4 +41,52 @@ class InsertNewData(tk.Frame):
                 print("Variety not found.")
 
         except FileNotFoundError:
-            print("CSV file not found.")
+            print("123123")
+
+    def load_external_file(self):
+        # Prompt the user to select a CSV file
+        file_path = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+
+        if file_path:
+            try:
+                # Read the file type and handle the case appropriately
+                if file_path.endswith(".csv"):
+                    df = pd.read_csv(file_path)
+                elif file_path.endswith(".xlsx"):
+                    df = pd.read_excel(file_path)
+                elif file_path.endswith(".txt"):
+                    df = pd.read_csv(file_path, sep="\t")
+                elif file_path.endswith(".json"):
+                    df = pd.read_json(file_path)
+                elif file_path.endswith(".xls"):
+                    df = pd.read_excel(file_path)
+                else:
+                    raise FileNotFoundError
+
+                # Load the selected CSV file
+                df = pd.read_csv(file_path)
+
+                # Update the existing database with the new data
+                try:
+                    db = pd.read_csv("../database/Varieties_Ground_Truth.csv")
+                    merged_db = pd.concat([db, df], ignore_index=True)
+                    merged_db.to_csv("../database/Varieties_Ground_Truth.csv", index=False)
+                except FileNotFoundError:
+                    # If the database doesn't exist yet, create a new one
+                    df.to_csv("../database/Varieties_Ground_Truth.csv", index=False)
+
+                print("File uploaded and merged with the database.")
+
+            except FileNotFoundError:
+                print("Selected file not found or is not in appropriate format.")
+
+
+if __name__ == "__main__":
+    app = tk.Tk()
+    app.geometry("800x400")  # Set the window size
+    app.title("Grape Variety Search")
+
+    frame = InsertNewData(app, None)
+    frame.pack(fill="both", expand=True)
+
+    app.mainloop()
